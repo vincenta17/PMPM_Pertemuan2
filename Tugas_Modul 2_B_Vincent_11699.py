a4c08bf5321addfaa9d397f290305651c35167b8
Vincent Alexander Li <151655134+vincenta17@users.noreply.github.com>
2024-09-25 23:08:28 +0700

    Tugas_Modul 2_B_Vincent_11699.py

--- Tugas_Modul 2_B_Vincent_11699.py
+++ Tugas_Modul 2_B_Vincent_11699.py
@@ -2,8 +2,7 @@ import streamlit as st
 import pickle
 import os
 
-model_path = 'D:\Semester 5\Pembelajaran Mesin Dan Mendalam\Tugas_B_11699'
-model=os.path.join(model_path,'GBT_heartDisease_model.pkl')
+model_path = 'GBT_heartDisease_model.pkl'
 
 with open(model,'rb') as f:
     loaded_model = pickle.load(f)
